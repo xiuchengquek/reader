c571ed107576c7c2fe3fd56427261f7655d555cc
xiuchengquek <x.quek@garvan.org.au>
2014-09-26 15:41:30 +1000

    Rejected commit: Default

--- reader.py
+++ reader.py
@@ -1,26 +1,53 @@
 __author__ = 'quek'
 
+
+def calculator(input):
+    pass
+
+
 def Reader(inputfile, linebreak):
     file_count = 0
     line_counter = 1
+    term_counter = 0
     outfile = "outdir/file_%i" % file_count
-    fh_out = open(outfile, 'w+')
+    fh_out = open(outfile, 'a+')
+    previous_term = False
+    previous_id = False
 
     with open(inputfile) as f:
         for lines in f.readlines():
-            current_id = lines.split('\t')[0]
-            if line_counter > linebreak :
-                if previous_id != current_id:
+            lines = lines.strip()
+            fields = lines.split('\t')
+            current_id = fields[0]
+            current_term = fields[-1]
+
+            if previous_term and current_term != previous_term:
+                previous_fields.append(str(term_counter))
+                out_line = "\t".join(previous_fields)
+                fh_out.write("%s\n" % out_line)
+                term_counter = 0
+                line_counter += 1
+
+            if line_counter > linebreak:
+                if previous_id != current_id and previous_id:
                     fh_out.close()
                     file_count += 1
-                    print 'Completed Approx %i' % ((file_count+1) * line_counter+1)
+                    print 'Completed Approx %i' % ((file_count + 1) * line_counter + 1)
                     outfile = "outdir/file_%i" % file_count
-                    fh_out = open(outfile, 'w+')
+                    fh_out = open(outfile, 'a+')
                     line_counter = 1
-            line_counter += 1
+
+            term_counter += 1
             previous_id = current_id
-            fh_out.write(lines)
+            previous_term = current_term
+            previous_fields = fields
+
+        previous_fields.append(str(term_counter))
+        out_line = "\t".join(previous_fields)
+        fh_out.write("%s\n" % out_line)
+
     fh_out.close()
+
 
 if __name__ == '__main__':
     Reader('mocktest.txt', 10)
--- tests.py
+++ tests.py
@@ -1,141 +1,57 @@
 __author__ = 'quek'
 
-
 import unittest
 import os
-from itertools import repeat
-
-
-
-
-
 
 from reader import Reader
 
 
-
-
-
-
 class TestDataFinder(unittest.TestCase):
-
     def setUp(self):
-        self.mock_file ='mockInput.txt'
+        self.mock_file = 'mockfiles/example_A.txt'
+        self.mock_expected = 'mockfiles/expected_A.txt'
         self.out_dir = os.path.join(os.getcwd(), 'outdir')
 
 
-    def testBasicInput(self):
-        mock_data = []
-        for x in range(1,11):
-            for y in range(1,11):
-                mock_data.append('%i\t%i' % (x,x))
-        mock_length = len(mock_data)
-        mock_data = "\n".join(mock_data)
+    def testExampleA(self):
+        self.maxDiff = None
 
-        with open(self.mock_file, 'w+') as f:
-            f.write(mock_data)
-        f.close()
-        self.assertEqual(mock_length,  sum(1 for line in open(self.mock_file)))
-        Reader(self.mock_file, 10)
+        Reader(self.mock_file, 20)
+        expect_output = 'outdir/file_0'
+        with open(self.mock_expected) as f:
+            mock_expected = f.read()
+
+        mock_expected = mock_expected.split('\n')
+
+        with open(expect_output) as f:
+            actual_output = f.read()
+        actual_output = actual_output.split('\n')
+
+        self.assertItemsEqual(mock_expected, actual_output)
+
+
+    def testExpectedB(self):
+        self.maxDiff = None
+        expected_files = ['outdir/file_0', 'outdir/file_1', 'outdir/file_2']
+        Reader(self.mock_file, 5)
+        expected_mock_files = ['mockfiles/expected_B_0.txt', 'mockfiles/expected_B_1.txt', 'mockfiles/expected_B_2.txt']
+        expected_contents = []
+        actual_contents = []
+
+        for i in expected_files:
+            with open(i, 'r') as f:
+                actual_contents.append(f.read())
+        for i in expected_mock_files:
+            with open(i, 'r') as f:
+                expected_contents.append(f.read())
+
         content = ["outdir/%s" % x for x in os.listdir('outdir') if not x.startswith('.')]
-        self.assertEqual(len(content), 10)
-        for x in content:
-            self.assertEqual(sum(1 for line in open(x)),10)
-
-
-
-    def testMorethanLineBreak(self):
-        mock_data = []
-        for x in range(1,11):
-            for y in range(1,21):
-                mock_data.append('%i\t%i' % (x,x))
-        mock_length = len(mock_data)
-        mock_data = "\n".join(mock_data)
-        with open(self.mock_file, 'w+') as f:
-            f.write(mock_data)
-            f.close()
-            self.assertEqual(mock_length,  sum(1 for line in open(self.mock_file)))
-            Reader(self.mock_file, 10)
-            content = ["outdir/%s" % x for x in os.listdir('outdir') if not x.startswith('.')]
-            self.assertEqual(len(content), 10)
-            for x in content:
-                self.assertEqual(sum(1 for line in open(x)),20)
-
-    def testFileNames(self):
-        expected_files = ['file_%i' % x for x in range(10)]
-        mock_data = []
-        for x in range(1,11):
-            for y in range(1,11):
-                mock_data.append('%i\t%i' % (x,x))
-        mock_length = len(mock_data)
-        mock_data = "\n".join(mock_data)
-
-        with open(self.mock_file, 'w+') as f:
-            f.write(mock_data)
-        f.close()
-        Reader(self.mock_file, 10)
-        content = [x for x in os.listdir('outdir') if not x.startswith('.')]
-        self.assertItemsEqual(sorted(expected_files), sorted(content))
-
-    def testOddList(self):
-        expected_files = ['1_10_1','11_20_2','21_30_3']
-        mock_data = []
-        for x in range(1,30):
-            mock_data.append('%i\t%i' % (x,x))
-        mock_length = len(mock_data)
-        mock_data = "\n".join(mock_data)
-
-        with open(self.mock_file, 'w+') as f:
-            f.write(mock_data)
-        f.close()
-        Reader(self.mock_file, 10)
-        content = ["outdir/%s" % x for x in os.listdir('outdir') if not x.startswith('.')]
-        self.assertEqual(len(content), 3)
-
-
-    def testOddName(self):
-        expected_files = ['file_0','file_1','file_2']
-        mock_data = []
-        for x in range(1,31):
-            mock_data.append('%i\t%i' % (x,x))
-        mock_length = len(mock_data)
-        mock_data = "\n".join(mock_data)
-
-        with open(self.mock_file, 'w+') as f:
-            f.write(mock_data)
-        f.close()
-        Reader(self.mock_file, 10)
-        content = [x for x in os.listdir('outdir') if not x.startswith('.')]
-        self.assertItemsEqual(sorted(expected_files), sorted(content))
-
-
-
-    def testFinal(self):
-        mock_data = list(repeat(6,5))
-        mock_data.extend(repeat(7,5))
-        mock_data.extend(repeat(8,6))
-        mock_data.extend(repeat(10,2))
-        mock_data.extend(repeat(12,14))
-        mock_data = [str(x) for x in mock_data]
-        mock_length = len(mock_data)
-
-        with open(self.mock_file, 'w+') as f:
-            for x in mock_data:
-                f.write('%s\t%s\n' % (x,x))
-        f.close()
-
-        Reader(self.mock_file, 10)
-        content = [x for x in os.listdir('outdir') if not x.startswith('.')]
-        self.assertEqual(len(content), 2)
-
-
-
-
+        self.assertItemsEqual(expected_files, content)
+        self.assertItemsEqual(expected_contents, actual_contents)
 
 
     def tearDown(self):
         content = ["outdir/%s" % x for x in os.listdir('outdir') if not x.startswith('.')]
-        os.unlink(self.mock_file)
         [os.unlink(x) for x in content]
 
 
